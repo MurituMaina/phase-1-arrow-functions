9b3073e4c3338e346ada91711c7ef21fcc87da85
MurituMaina <vincent.maina@student.moringaschool.com>
2022-08-18 00:39:46 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+//function expression divide
+let num1 = 2000;
+let num2 =100;
+const divide = function () {
+    return num1/num2;
+}
+console.log(divide());
+
+//Arrow function square
+let x =2;
+const square = x => x * x;
+console.log(square(x));
+
+//arrow function add
+let a =3;
+let b= 4;
+const add = (a,b) => a+b;
+console.log(add(a,b));
